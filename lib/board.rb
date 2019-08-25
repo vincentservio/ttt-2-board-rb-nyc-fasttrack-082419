6029a0dc71a3cd564d866468ff4b21082c6f6b62
vincent servio <vincentservio@gmail.com>
2019-08-25 02:02:21 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,10 +1,1 @@
-
-puts board = [" "," "," "," "," "," "," "," "," ",]
-
-def display_board 
-  puts row =  ["  " "|" "   " "|" " "]
-  puts separator = "___________"
-  puts row 
-  puts separator
-  puts row 
-end 
+board = [" "," "," "," "," "," "," "," "," "]
